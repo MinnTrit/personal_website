7fd336d59f2e006e14b2b365252aed43d2955301
root <root@DESKTOP-7J9DIND.>
2025-01-31 05:06:02 +0000

    fix scrolling

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask, request, render_template
-app = Flask(__name__)
+from flask import Flask, request, render_template, url_for
+app = Flask(__name__, static_folder='static')
 app.debug=True
+app.add_url_rule('/favicon.ico',
+                 redirect_to=url_for('static', filename='engineering.ico'))
 #Initialize common variables
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
         return render_template('index.html')
+
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5003, debug=False)
